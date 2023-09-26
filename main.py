177a877dd0b1912a2a4a24e0ea59200fc093d9e2
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-26 20:30:47 -0300

    feat: get + delete by id

--- main.py
+++ main.py
@@ -20,9 +20,26 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-	return {"message": "Hello, world!"}
+	return {"message": "Welcome to cars API!"}
 
-
+# GET ALL CARS
 @app.get('/api/v1/cars')
 async def get_cars():
     return db
+
+# GET CAR BY ID
+@app.get('/api/v1/cars/{car_id}')
+async def get_car(car_id: int):
+    for car in db:
+        if car.id == car_id:
+            return car
+    return HTTPException(404, f"No such car with id: {car_id}")
+
+# DELETE CAR
+@app.delete('/api/v1/cars/{car_id}')
+async def delte_car(car_id: int):
+    for car in db:
+        if car.id == car_id:
+            db.remove(car)
+            return {'on_delete': 'success'}
+    return HTTPException(404, f"No such car with id: {car_id}")

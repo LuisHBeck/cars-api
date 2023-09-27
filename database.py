55ddfa9735bff0589d524c5efb630dc5baf8b8e5
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-26 22:23:50 -0300

    feat: use databise

---
+++ database.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from sqlmodel import SQLModel, Field, create_engine
+
+DB_FILE = 'db.sqlite3'
+engine = create_engine(f'sqlite:///{DB_FILE}', echo=True)
+
+
+class CarModel(SQLModel, table=True):
+	id: Optional[int] = Field(default=None, primary_key=True)
+	manufacturer: str
+	model: str
+	year: int
+	motorization: float
+	fuel: str
+	avg_consumption: Optional[float] = None
+	plate_number: str
+
+
+def create_tables():
+	SQLModel.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+	create_tables()
--- main.py
+++ main.py
@@ -1,64 +1,68 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, Response
 from typing import List, Union
-
+from sqlmodel import Session, select
+from database import engine, CarModel
 from models import Car, Fuel
 
-db: List[Car] = [
-    Car(
-        id=1,
-        manufacturer='Honda',
-        model='Civic',
-        year=2016,
-        motorization=2.0,
-        fuel=Fuel.flex,
-        avg_consumption=9.0,
-        plate_number='EAA-3719'
-    )
-]
-
 app = FastAPI()
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 @app.get('/')
 async def root():
 	return {"message": "Welcome to cars API!"}
 
 # GET ALL CARS
-@app.get('/api/v1/cars/')
-async def get_cars():
-    return db
-
+@app.get('/api/v1/cars/') 
+async def get_cars(session: Session() = Depends(get_session)):
+    statement = select(CarModel)
+    result = session.exec(statement).all()
+    return result
+    
 # GET CAR BY ID
 @app.get('/api/v1/cars/{car_id}/', response_model=Car)
-async def get_car(car_id: int):
-    for car in db:
-        if car.id == car_id:
-            return car
+async def get_car(car_id: int, session: Session() = Depends(get_session)):
+    car = session.get(CarModel, car_id)
+    if car is not None:
+        return car
     raise HTTPException(404, f"No such car with id: {car_id}")
 
 # CREATE NEW CAR
 @app.post('/api/v1/cars/', response_model=Car, status_code=201)
-async def post_car(car: Car):
-    car.id = len(db) + 1
-    db.append(car)
+async def post_car(car: CarModel, session: Session() = Depends(get_session)):
+    session.add(car)
+    session.commit()
+    session.refresh(car)
     return car
 
 # UPDATE CAR INFO
 @app.put('/api/v1/cars/{car_id}/', response_model=Car)
-async def put_car(car_id: int, update_data: Car):
-    for car in db:
-        if car.id == car_id:
-            for key, value in update_data.model_dump().items():
-                if key != 'id':
-                    setattr(car, key, value)
-            return car
-    raise HTTPException(404, f"No such car with id: {car_id}")
+async def put_car(car_id: int, update_data: Car, session: Session() = Depends(get_session)):
+    car = session.get(CarModel, car_id)
+    car_dict = update_data.dict(exclude_unset=True)
 
+    if car is None: 
+        raise HTTPException(404, f"No such car with id: {car_id}")
+    
+    for key, value in car_dict.items():
+        setattr(car, key, value)
+    session.add(car)
+    session.commit()
+    session.refresh(car)
+    return car
 
 # DELETE CAR
 @app.delete('/api/v1/cars/{car_id}/')
-async def delte_car(car_id: int):
-    for car in db:
-        if car.id == car_id:
-            db.remove(car)
-            return {'on_delete': 'success'}
-    raise HTTPException(404, f"No such car with id: {car_id}")
+async def delte_car(car_id: int, session: Session() = Depends(get_session)):
+    car = session.get(CarModel, car_id)
+
+    if car is None: 
+        raise HTTPException(404, f"No such car with id: {car_id}")
+    
+    session.delete(car)
+    session.commit()
+    return Response(status_code=200)
